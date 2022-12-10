f6b8a2e0a7ab1bde58912a0b7b8a335b90cf04e5
Yumaka Lukasz <120280121+ymkxymk@users.noreply.github.com>
2022-12-11 01:55:39 +0500

    Create randomai.py

---
+++ randomai.py
@@ -0,0 +1,8 @@
+class Rand1():   
+   def rand1():
+      a = [128,8249,8184,8101,183,93,742]
+      b = ["JqjJwj","JwjWoz","NaPw","BdoQo","DiaIa"]
+      c = a[0:7]+b[0:5]
+   print(c)
+
+# The easy token generator use: from randomai import rand1
